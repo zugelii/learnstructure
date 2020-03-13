3fc3cd1f403e04f7a5d2998875ab97a21f23099f
yulu <yulu.li@continental.com>
2020-03-14 01:20:08 +0800

    get system time

--- casetest/main.cpp
+++ casetest/main.cpp
@@ -10,6 +10,10 @@
 #include <atomic>
 #include <unistd.h>
 #include <sys/syscall.h>//Linux system call for thread id
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+
 using namespace::std;
 
 
@@ -19,12 +23,25 @@ void on_signal_int(int signum)
 	(void)signum;
 	main_thread_run = false;
 }
+// Get current date/time, format is YYYY-MM-DD.HH:mm:ss
+const std::string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
+    return buf;
+}
+
+
 int main(int argc, char**argv)
 {
-    pid_t p = getpid();
-    std::cout << "onradar pid:" << p << std::endl;
-    printf("child thread lwpid = %u\n", syscall(SYS_gettid));
-    printf("child thread tid = %u\n", pthread_self());
-    sleep(10);
+    string st(__TIMESTAMP__);
+    auto t = chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    auto a = put_time(localtime(&t), "%Y-%m-%d %X");
+    cout << a << endl;
+    cout << currentDateTime() << endl; 
 
 }
