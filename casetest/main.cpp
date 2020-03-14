cef5b613d0fc1b3599023b91e8f1e0e5609cf3d4
yulu <yulu.li@continental.com>
2020-03-14 19:11:56 +0800

    how to get aimed system time

--- casetest/main.cpp
+++ casetest/main.cpp
@@ -13,6 +13,8 @@
 #include <chrono>
 #include <ctime>
 #include <iomanip>
+#include <sstream>
+#include <map>
 
 using namespace::std;
 
@@ -23,7 +25,10 @@ void on_signal_int(int signum)
 	(void)signum;
 	main_thread_run = false;
 }
-// Get current date/time, format is YYYY-MM-DD.HH:mm:ss
+
+
+
+// Get current date/time, format is YYYY-MM-DD.HH:mm:ss , this can translate long time to aimed format
 const std::string currentDateTime() {
     time_t     now = time(0);
     struct tm  tstruct;
@@ -36,12 +41,19 @@ const std::string currentDateTime() {
 }
 
 
-int main(int argc, char**argv)
+void get_aimed_format_time()
 {
     string st(__TIMESTAMP__);
+    cout << "st:" << st << endl;  //get system time from compiler
     auto t = chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     auto a = put_time(localtime(&t), "%Y-%m-%d %X");
-    cout << a << endl;
-    cout << currentDateTime() << endl; 
+    cout << a << endl; //get system time from chrono ,then change to aimed format
+    cout << currentDateTime() << endl;
+}
+
+int main(int argc, char**argv)
+{
+ 
+    get_aimed_format_time();//
 
 }
