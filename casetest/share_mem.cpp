38211de385f998995904e0d661faaa6a926bf31c
yulu <yulu.li@continental.com>
2020-03-22 19:13:41 +0800

    add base case for share memory test

---
+++ casetest/share_mem.cpp
@@ -0,0 +1,45 @@
+
+#include "share_mem.h"
+
+
+
+
+#define MMAP_DATA_SIZE 1024
+ 
+int test_share_mem()
+{
+        char * data;
+        int fd = shm_open("shm-file0001", O_CREAT|O_RDWR, 0777);
+ 
+        if (fd < 0) 
+        {
+            printf("shm_open failed!\n");
+            return -1;
+        }
+ 
+        ftruncate(fd, MMAP_DATA_SIZE);
+        if (1) 
+        {
+            data = (char*)mmap(NULL, 1024, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+            if (!data) 
+            {
+                printf("mmap failed\n");
+                close(fd);
+            }
+            sprintf(data,"This is a share memory! %d\n",fd);
+            munmap(data,MMAP_DATA_SIZE);
+        }
+        else 
+        {
+            char buf[1024];
+            int len = sprintf(buf,"This is a share memory by write! ! %d\n",fd);
+            if (write(fd,buf,len) <= 0) 
+            {
+                printf("write file %d failed!%d\n",len, errno);
+            }  
+        } 
+        close(fd); 
+        //shm_unlink("shm-file0001");
+ 
+        return 0;
+}
---
+++ casetest/share_mem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+int test_share_mem();
